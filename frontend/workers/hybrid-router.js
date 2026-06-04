5fc30ac3ed652c800568a6c4cd9e7f6903e1ed99
Okubo Kosuke <okb@OkubonoMac-mini.local>
2026-06-04 20:21:30 +0900

    Point router to BEYOOOOONDS member maker deploy

--- frontend/workers/hybrid-router.js
+++ frontend/workers/hybrid-router.js
@@ -1,4 +1,4 @@
-const CALLS_ORIGIN = "9604240e.hello-project-jp.pages.dev";
+const CALLS_ORIGIN = "90df01e5.hello-project-jp.pages.dev";
 const SHINDAN_ORIGIN = "a044a678.hello-project-jp.pages.dev";
 
 function isShindanRoute(pathname) {
